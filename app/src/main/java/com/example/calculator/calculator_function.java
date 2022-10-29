a6ee429497a884e75261a7c47833f229fe9af1c7
dzeno <dzenan.biznis@gmail.com>
2022-10-29 12:41:11 +0200

    Dorada funkcija za pretragu i razlaganje formule preuzete iz stringa na lijevu i desnu stranu od prvog znaka na koji dodje i tako radi operacije.
    Potrebno dodati:
    -Prioritete u racunskim radnjama
    -Button ekstenzije funkcija (sqrt, sin, cos...)
    -Fixat bugove
    -Prebaciti vrijednosti iz Int u float/double radi sigurnosti

--- app/src/main/java/com/example/calculator/calculator_function.java
+++ app/src/main/java/com/example/calculator/calculator_function.java
@@ -1,6 +1,6 @@
 package com.example.calculator;
 
-import java.lang.Math;;
+import java.lang.Math;
 
 public class calculator_function {
 
@@ -47,17 +47,12 @@ public class calculator_function {
         return false;
     }
 
-    public static int poweer(int a, int b){
-        int temp = a;
-        for(int i = 0; i < b - 1; i++){
-            temp = temp * temp;
-        }
-
-        return temp;
-    }
-
     public static void Provjera(int i, char znak){
         String lijeviBroj = "";
+        String lijeviBroj_original = "";
+        String pamtim_broj = "";
+        String novi_tempic = "";
+
         String desniBroj = "";
         String original_tempic = tempic;
 
@@ -66,68 +61,79 @@ public class calculator_function {
         int temp3 = 1;
         int temp_index = 0;
 
-        //lijeva strana
-        for(int x = 0; x<i; x++){
-            if(isNumeric(tempic.charAt(x)))
-                lijeviBroj = lijeviBroj + tempic.charAt(x);
-            else{
-                if(tempic.charAt(x) == '-'){
-                    lijeviBroj = "-";
-                }else
-                    break;
+        boolean provjera_desnog_predzanka = false;
+        int index_predznaka = 0;
+
+
+            //lijeva strana                                     //POSTAVITI DRUGACIJE LIJEVU STRANU KAKO BI OPTIMIZOVAO FLOW
+            for(int x = 0; x<i; x++){
+                if(isNumeric(tempic.charAt(x)))
+                    lijeviBroj = lijeviBroj + tempic.charAt(x);
+                else{
+                    if(tempic.charAt(0) == '-'){
+                        lijeviBroj_original = lijeviBroj;
+                        lijeviBroj = "-" + lijeviBroj_original;
+                    }else
+                        break;
+                }
+
             }
 
-        }
-        //desna strana
-        for(int x = i + 1; x<tempic.length(); x++){
-            if(isNumeric(tempic.charAt(x))) {
-                desniBroj = desniBroj + tempic.charAt(x);
-            }
-            else{
-                if(tempic.charAt(x) == '-'){
-                    desniBroj = "-";
-                }else{
-                    temp_index = x;
-                    break;
+            //desna strana
+
+            for(int x = i + 1; x<tempic.length(); x++){
+                if(isNumeric(tempic.charAt(x))) {
+                    desniBroj = desniBroj + tempic.charAt(x);
+                }
+                else{
+                    if(tempic.charAt(i + 1) == '-'){
+                        desniBroj = "-";
+                    }else{
+                        temp_index = x;
+                        break;
+                    }
                 }
             }
-        }
-
-        tempBroj = Integer.parseInt(lijeviBroj);
-        tempBroj2 = Integer.parseInt(desniBroj);
-
-        //DODAVANJE PO ZNAKU
-        if(znak == '+'){
-            temp3 = tempBroj + tempBroj2;
-        }
-        else if(znak == '*'){
-            temp3 = tempBroj * tempBroj2;
-        }
-        else if(znak == '*'){
-            temp3 = tempBroj * tempBroj2;
-        }
-        else if(znak == '/'){
-            temp3 = tempBroj / tempBroj2;
-        }
-        else if(znak == '-'){
-            temp3 = tempBroj - tempBroj2;
-        }
-        else if(znak == '^'){
-            temp3 = (int) Math.pow((double) tempBroj, (double) tempBroj2);
-        }
-
-        String novi_tempic = ((Integer)temp3).toString();
-        if(temp_index > 0){
-            tempic = novi_tempic + original_tempic.substring(temp_index);
-            brojac = 0;
-        }else{
-            tempic = novi_tempic;
-            provjera = false;
-        }
 
 
+            tempBroj = Integer.parseInt(lijeviBroj);
+            tempBroj2 = Integer.parseInt(desniBroj);
 
-        //brojac = 0;
+            //DODAVANJE PO ZNAKU
+            if(znak == '+'){
+                temp3 = tempBroj + tempBroj2;
+            }
+            else if(znak == '*'){
+                temp3 = tempBroj * tempBroj2;
+            }
+            else if(znak == '*'){
+                temp3 = tempBroj * tempBroj2;
+            }
+            else if(znak == '/'){
+                temp3 = tempBroj / tempBroj2;
+            }
+            else if(znak == '-'){
+                temp3 = tempBroj - tempBroj2;
+            }
+            else if(znak == '^'){
+                temp3 = (int) Math.pow((double) tempBroj, (double) tempBroj2);
+            }
 
-    }
+            novi_tempic = ((Integer)temp3).toString();
+            if(temp_index > 0){
+                tempic = novi_tempic + original_tempic.substring(temp_index);
+                brojac = 0;
+            }else{
+                tempic = novi_tempic;
+                provjera = false;
+            }
+
+
+
+            //brojac = 0;
+
+
+        }
+
+
 }
